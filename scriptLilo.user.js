6e9311260521dc82a57c8c52ce1ec9db38717da3
Antton Dev <64503329+antton-dev@users.noreply.github.com>
2021-03-20 21:30:27 +0100

    Create scriptLilo.user.js

---
+++ scriptLilo.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Lilo Qwant Redirection
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description   Change la redirection vers un autre moteur de recherche. 
+// @author       AnttonDev
+// @match        *https://search.lilo.org/*
+// @icon         https://www.lilo.org/wp-content/themes/jarvis_wp/ajans/assets/lilo-logo.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const searchParams = new URLSearchParams(window.location.search)
+    searchParams.has('q')
+    const query = searchParams.get('q')
+
+    const newLinkToQwant = "https://www.qwant.com/?q=" + query + "&t=web"
+
+    const redirectLink = document.querySelector('.menu-google a');
+    redirectLink.textContent = 'Qwant';
+    redirectLink.removeAttribute("onclick");
+    redirectLink.removeAttribute("title");
+    redirectLink.removeAttribute("href");
+    redirectLink.setAttribute('title', 'Tentez votre chance avec Qwant !');
+    redirectLink.setAttribute('href', newLinkToQwant);
+    redirectLink.setAttribute('target', '_blank');
+})();
